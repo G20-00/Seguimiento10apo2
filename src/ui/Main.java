068932468483f20409828bd9eb88cd53e7ae654d
unknown <70019070+G20-00@users.noreply.github.com>
2021-04-15 18:16:11 -0500

    finalizado
    LEE DE CONSOLA

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,9 +1,8 @@
 package ui;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 
@@ -14,28 +13,24 @@ public class Main{
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	public static void main(String[] args) throws NumberFormatException, IOException{
 		
-		//BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
 		int n = Integer.parseInt(br.readLine());
 		repit(n);
 		
 	}
 	static void repit(int j) throws NumberFormatException, IOException {
 		if(j>0) {
-			System.out.println("\n" );
 			int n = Integer.parseInt(br.readLine());
 			torres = new int [3];
 			torres[0]=n;
 			torresHanoi(n,1,3,2,1);
 			j--;
-			System.out.println(j);
 			repit(j);
 		}
 	}
 	static void torresHanoi(int n,int o,int d,int aux,int cont){
-		
 		if(n>0){
 			if(cont<=1) {
-				System.out.println(torres[0]+ " " + torres[1] + " "+torres[2]);
+				System.out.println("\n"+torres[0]+ " " + torres[1] + " "+torres[2]);
 			}
 			torresHanoi(n-1,o,aux,d,++cont);
 			
