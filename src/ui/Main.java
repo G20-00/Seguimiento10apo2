ca92040234ba036e9b08881ba2707a390b5772f1
unknown <70019070+G20-00@users.noreply.github.com>
2021-04-15 16:47:59 -0500

    final

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,40 +1,53 @@
 package ui;
-	import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 
-	public class Main{
+public class Main{
+
+	static int [] torres = new int [3];
+	static Scanner entrada = new Scanner(System.in);
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws NumberFormatException, IOException{
 		
-		static int [] torres = new int [3];
-		static Scanner entrada = new Scanner(System.in);
-		static void torresHanoi(int n,int o,int d,int aux){
-			 
-
-			if(n>0){
-				torresHanoi(n-1,o,aux,d);
-				System.out.println(torres[0]+ " " + torres[1] + " "+torres[2]);
-				torres[o-1]--;
-				torres[d-1]++;	
-				torresHanoi(n-1,aux,d,o);
-				if(n==1) {
-					System.out.println(torres[0]+ " " + torres[1] + " "+torres[2]);
-				}
-
-			}
-			
-			
-		}
-		public static void main(String[] args){
-			int n;
-			
-			
-			//while (en.hasMoreElements()) {
-				//type type = (type) en.nextElement();
-				
-			//}
-			n=entrada.nextInt();
+		//BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		repit(n);
+		
+	}
+	static void repit(int j) throws NumberFormatException, IOException {
+		if(j>0) {
+			System.out.println("\n" );
+			int n = Integer.parseInt(br.readLine());
 			torres = new int [3];
 			torres[0]=n;
-			torresHanoi(n,1,3,2);
+			torresHanoi(n,1,3,2,1);
+			j--;
+			System.out.println(j);
+			repit(j);
 		}
 	}
+	static void torresHanoi(int n,int o,int d,int aux,int cont){
+		
+		if(n>0){
+			if(cont<=1) {
+				System.out.println(torres[0]+ " " + torres[1] + " "+torres[2]);
+			}
+			torresHanoi(n-1,o,aux,d,++cont);
+			
+			torres[o-1]--;
+			torres[d-1]++;
+			System.out.println(torres[0]+ " " + torres[1] + " "+torres[2]);
+			cont++;
+			torresHanoi(n-1,aux,d,o,cont);
+		
+		}
+	}
+	
+
+}
 
