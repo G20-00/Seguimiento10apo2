c49d34a585104c55d4b4c15f96eebabd7c13d271
unknown <70019070+G20-00@users.noreply.github.com>
2021-04-16 11:45:11 -0500

    escritura de archivo

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,53 +1,83 @@
 package ui;
+
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
+public class Main {
 
-public class Main{
-
-	static int [] torres = new int [3];
+	static int[] torres = new int[3];
 	static Scanner entrada = new Scanner(System.in);
-	static BufferedReader br ;
-	static PrintWriter pw ;
-	public static void main(String[] args) throws NumberFormatException, IOException{
-		 br = new BufferedReader(new FileReader("C:\\Users\\Pc\\Desktop\\ejemplo\\hola.txt"));
-		 pw = new PrintWriter("C:\\Users\\Pc\\Desktop\\ejemplo\\output.txt");
+	static BufferedReader br;
+	// static PrintWriter pw ;
+	static FileOutputStream ou;
+	static String output = "";
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		File f= new File("data/outputExcel.csv");
+		br = new BufferedReader(new FileReader("data/hola.txt"));
+		// pw = new PrintWriter("C:\\Users\\Pc\\Desktop\\ejemplo\\outputExcel.csv");
+		ou = new FileOutputStream(f);
 		int n = Integer.parseInt(br.readLine());
-		repit(n);
-		
+		int cont = 1;
+
+		repit(n, cont);
+
 	}
-	static void repit(int j) throws NumberFormatException, IOException {
-		if(j>0) {
+
+	static void repit(int j, int cont) throws NumberFormatException, IOException {
+		if (j > 0) {
 			int n = Integer.parseInt(br.readLine());
-			torres = new int [3];
-			torres[0]=n;
-			torresHanoi(n,1,3,2,1);
+			torres = new int[3];
+			torres[0] = n;
+			torresHanoi(n, 1, 3, 2, 1, cont);
 			j--;
-			repit(j);
-		}
-	}
-	static void torresHanoi(int n,int o,int d,int aux,int cont){
-		if(n>0){
-			if(cont<=1) {
-				System.out.println("\n"+torres[0]+ " " + torres[1] + " "+torres[2]);
-				pw.println("\n"+torres[0]+ " " + torres[1] + " "+torres[2]);
-			}
-			torresHanoi(n-1,o,aux,d,++cont);
-			
-			torres[o-1]--;
-			torres[d-1]++;
-			pw.println(torres[0]+ " " + torres[1] + " "+torres[2]);
-			System.out.println(torres[0]+ " " + torres[1] + " "+torres[2]);
-			
 			cont++;
-			torresHanoi(n-1,aux,d,o,cont);
-		
+			repit(j, cont);
+		} else {
+			// pw.println(output);
+			ou.close();
 		}
 	}
-	
+
+	static void torresHanoi(int n, int o, int d, int aux, int cont, int cont2) throws IOException {
+		String auxiliar = "";
+		if (n > 0) {
+			if (cont <= 1) {
+				if (cont2 == 1) {
+					System.out.println(torres[0] + " " + torres[1] + " " + torres[2]);
+					output += (torres[0] + " " + torres[1] + " " + torres[2] + "\n");
+					auxiliar = torres[0] + " " + torres[1] + " " + torres[2] + "\n";
+					ou.write(auxiliar.getBytes());
+					// pw.println(torres[0]+ " " + torres[1] + " "+torres[2]);
+					cont2++;
+				} else {
+					System.out.println("\n" + torres[0] + " " + torres[1] + " " + torres[2]);
+					output += "\n" + torres[0] + " " + torres[1] + " " + torres[2] + "\n";
+					auxiliar = "\n" + torres[0] + " " + torres[1] + " " + torres[2] + "\n";
+					ou.write(auxiliar.getBytes());
+					// pw.println("\n"+torres[0]+ " " + torres[1] + " "+torres[2]);
+				}
+			}
+			torresHanoi(n - 1, o, aux, d, ++cont, cont2);
+
+			torres[o - 1]--;
+			torres[d - 1]++;
+
+			// pw.println(torres[0]+ " " + torres[1] + " "+torres[2]);
+			System.out.println(torres[0] + " " + torres[1] + " " + torres[2]);
+			output += torres[0] + " " + torres[1] + " " + torres[2] + "\n";
+			auxiliar = torres[0] + " " + torres[1] + " " + torres[2] + "\n";
+			ou.write(auxiliar.getBytes());
+			cont++;
+			torresHanoi(n - 1, aux, d, o, cont, cont2);
+
+		}
+	}
 
 }
-
