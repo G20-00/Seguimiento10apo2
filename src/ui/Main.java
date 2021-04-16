f8abee57a45cfbdcd3a60453c5619feae6dd4341
unknown <70019070+G20-00@users.noreply.github.com>
2021-04-15 20:32:38 -0500

    importando

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,8 +1,8 @@
 package ui;
 import java.io.BufferedReader;
-
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 
@@ -10,9 +10,11 @@ public class Main{
 
 	static int [] torres = new int [3];
 	static Scanner entrada = new Scanner(System.in);
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader br ;
+	static PrintWriter pw ;
 	public static void main(String[] args) throws NumberFormatException, IOException{
-		
+		 br = new BufferedReader(new FileReader("C:\\Users\\Pc\\Desktop\\ejemplo\\hola.txt"));
+		 pw = new PrintWriter("C:\\Users\\Pc\\Desktop\\ejemplo\\output.txt");
 		int n = Integer.parseInt(br.readLine());
 		repit(n);
 		
@@ -31,12 +33,15 @@ public class Main{
 		if(n>0){
 			if(cont<=1) {
 				System.out.println("\n"+torres[0]+ " " + torres[1] + " "+torres[2]);
+				pw.println("\n"+torres[0]+ " " + torres[1] + " "+torres[2]);
 			}
 			torresHanoi(n-1,o,aux,d,++cont);
 			
 			torres[o-1]--;
 			torres[d-1]++;
+			pw.println(torres[0]+ " " + torres[1] + " "+torres[2]);
 			System.out.println(torres[0]+ " " + torres[1] + " "+torres[2]);
+			
 			cont++;
 			torresHanoi(n-1,aux,d,o,cont);
 		
